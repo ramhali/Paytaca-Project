99bac6fc71846a8702fc6235f8eef771da555a2f
wilzxcs <wilbendy1107@gmail.com>
2024-06-26 11:45:02 +0800

    comment from wilz

--- PaytacaPaymentGateway/api/views.py
+++ PaytacaPaymentGateway/api/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
+##Comment
 class AccountViewAPI(APIView):
     def get(self, request):
         requestData = request.data
