3f50bffad578e31a4e6467803b6ffb9c13f535a5
Wilzxcs <wilbendy1107@gmail.com>
2024-07-19 16:13:19 +0800

    modified:   src/pages/PayApi.vue

--- src/pages/PayApi.vue
+++ src/pages/PayApi.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <q-page padding>
     <div v-if="!isPaid" class="q-pa-md column flex-center relative-center">
       <div class="column q-mb-md">
@@ -45,7 +45,8 @@
 
         <q-card-section>
           <div class="q-my-sm text-h6 flex flex-center text-weight-bolder" style="color: #39a848;">Paytaca</div>
-          <div class="text-subtitle1 flex flex-center text-weight-regular" style="color: #39a848;">Your Money, Your Control</div>
+          <div class="text-subtitle1 flex flex-center text-weight-regular" style="color: #39a848;">Your Money, Your
+            Control</div>
         </q-card-section>
       </q-card>
     </div>
@@ -56,13 +57,14 @@
 import { api } from 'src/boot/axios';
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 import { useURLStore } from 'src/stores/urlstore';
+import axios from 'axios';
 
 const urlstore = useURLStore();
-const url = urlstore.getterUrl;
 
 let addressData = ref(null);
 let amountData = ref(null);
 let descData = ref(null);
+
 
 const time = ref(120);
 const timer = ref(null);
@@ -71,158 +73,45 @@ const pollTimer = ref(null);
 let code = ref(null);
 let isPaid = ref(false);
 
-const getResponse = async () => {
-  const response = await api.get(`${url}`);
-  addressData.value = response.data.address;
-  amountData.value = response.data.amount_bch;
-  descData.value = response.data.desc;
-  isPaid.value = response.data.paid;
+const getQueryParams = async () => {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  const queryParams = Object.fromEntries(params);
 
-  console.log("Response Data:\n", response.data);
+  console.log(url.search);
+  console.log(queryParams);
 
-  const currDate = new Date(response.data.created_at);
-  const newDate = new Date(currDate.getTime() + 2 * 60 * 1000);
-
-  console.log("Current Date: ", currDate);
-  console.log("New Date: ", newDate);
-
-  startTimer(newDate);
-  generateQrCode(addressData.value, amountData.value);
-
-};
-
-const generateQrCode = (addressData, amountData) => {
-  code.value = `${addressData}?amount=${amountData}`;
-  console.log("Code used to generate QR", code.value);
-};
-
-const formattedTime = computed(() => {
-  const minutes = Math.floor(time.value / 60);
-  const seconds = time.value % 60;
-  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-});
-
-const startTimer = (endDate) => {
-  if (timer.value) return; // Prevent multiple intervals
-  timer.value = setInterval(() => {
-    const now = new Date();
-    const remainingTime = Math.max(0, endDate - now);
-    time.value = Math.floor(remainingTime / 1000);
-
-    if (remainingTime <= 0) {
-      clearInterval(timer.value);
-      timer.value = null;
-      urlstore.removeURL();
-    }
-  }, 1000);
-};
-
-const checkPaymentStatus = async () => {
-  const response = await api.get(`${url}`);
-  isPaid.value = response.data.paid;
-  if (isPaid.value) {
-    clearInterval(pollTimer.value);
-    pollTimer.value = null;
-
+  const urldata = {
+    token: queryParams.token,
+    amount: queryParams.amount,
+    currency: queryParams.currency,
+    desc: queryParams.desc
   }
-};
 
-onMounted(() => {
-  getResponse();
-  pollTimer.value = setInterval(checkPaymentStatus, 5000); // Poll every 5 seconds
-});
+  try {
+    const response = await axios.get('http://localhost:8000/pay', {
+      params: {
+        token: urldata.token,
+        amount: urldata.amount,
+        currency: urldata.currency,
+        desc: urldata.desc,
+      }
+    });
 
-onUnmounted(() => {
-  clearInterval(timer.value);
-  clearInterval(pollTimer.value);
-  urlstore.removeURL();
-});
-</script>
+    const fetchedURL = response.data.url;
 
-<style scoped>
-.ellipsis {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+    console.log("Fetched URL",fetchedURL);
+    urlstore.storeURL(fetchedURL);
+
+    console.log(fetchedURL);
+    getResponse(fetchedURL)
+  } catch (error) {
+    console.error(error);
+  }
 }
-</style>
- -->
 
- <template>
-  <q-page padding>
-    <div v-if="!isPaid" class="q-pa-md column flex-center relative-center">
-      <div class="column q-mb-md">
-        <label>Scan the qr code below before the address expires</label>
-      </div>
-
-      <q-card class="my-card shadow-6">
-        <q-card-section class="q-pb-sm flex flex-center">
-          <transition appear leave-active-class="animated flipOutY slower">
-            <vue-qrcode v-if="code !== null" class="q-mt-md" :value="code" :size="250" level="H" />
-          </transition>
-        </q-card-section>
-
-        <q-card-section class="q-py-sm q-px-md row flex-center q-col-gutter-xs">
-          <q-btn flat no-caps color="accent" icon="content_copy">
-            <div class="ellipsis" style="width: 200px">{{ addressData }}</div>
-          </q-btn>
-        </q-card-section>
-
-        <q-card-section class="q-pt-lg q-pb-xs">
-          <div class="row items-center">
-            <q-linear-progress size="5px" :value="time / 120" color="accent" class="q-mr-md" />
-          </div>
-        </q-card-section>
-
-        <q-card-section>
-          <div class="q-my-sm text-h6 flex flex-center text-weight-regular">{{ descData }}</div>
-          <div class="text-subtitle1 flex flex-center text-weight-bold">BCH {{ amountData }}</div>
-        </q-card-section>
-      </q-card>
-    </div>
-
-    <div v-else class="q-pa-md column flex-center relative-center">
-      <div class="column q-mb-md">
-        <label>Transaction Done!</label>
-      </div>
-
-      <q-card class="my-card shadow-6">
-        <q-card-section class="q-pa-none">
-          <transition appear enter-active-class="animated flipInY slower">
-            <q-img src="/src/assets/images/paid_logo.png" alt="PAID" height="250px" width="250px" />
-          </transition>
-        </q-card-section>
-
-        <q-card-section>
-          <div class="q-my-sm text-h6 flex flex-center text-weight-bolder" style="color: #39a848;">Paytaca</div>
-          <div class="text-subtitle1 flex flex-center text-weight-regular" style="color: #39a848;">Your Money, Your Control</div>
-        </q-card-section>
-      </q-card>
-    </div>
-  </q-page>
-</template>
-
-<script setup>
-import { api } from 'src/boot/axios';
-import { computed, onMounted, onUnmounted, ref } from 'vue';
-import { useURLStore } from 'src/stores/urlstore';
-
-const urlstore = useURLStore();
-const url = urlstore.getterUrl;
-
-let addressData = ref(null);
-let amountData = ref(null);
-let descData = ref(null);
-
-const time = ref(120);
-const timer = ref(null);
-const pollTimer = ref(null);
-
-let code = ref(null);
-let isPaid = ref(false);
-
-const getResponse = async () => {
-  const response = await api.get(`${url}`);
+const getResponse = async (urldata) => {
+  const response = await api.get(`${urldata}`);
   addressData.value = response.data.address;
   amountData.value = response.data.amount_bch;
   descData.value = response.data.desc;
@@ -270,7 +159,7 @@ const startTimer = (endDate) => {
 };
 
 const checkPaymentStatus = async () => {
-  const response = await api.get(`${url}`);
+  const response = await api.get(`${urlstore.getUrl}`);
   isPaid.value = response.data.paid;
   if (isPaid.value) {
     clearInterval(pollTimer.value);
@@ -279,7 +168,7 @@ const checkPaymentStatus = async () => {
 };
 
 onMounted(() => {
-  getResponse();
+  getQueryParams()
   pollTimer.value = setInterval(checkPaymentStatus, 5000); // Poll every 5 seconds
 });
 
